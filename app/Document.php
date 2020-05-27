602f7ce88504092a82f2f80e80281db4d539225c
Remote Software Developer <39113714+remote-software-dev@users.noreply.github.com>
2020-05-27 11:32:57 +0700

    fixed minor changes

--- app/Document.php
+++ app/Document.php
@@ -7,9 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Document extends Model
 {
     protected $table = 'documents';
-    protected $fillable = ['name', 'extension', 'size'];
-
-    public function getAttributeSize($value){
-        return number_format($value/1024, 1);
-    }
+    protected $fillable = ['name', 'picture',];
 }
